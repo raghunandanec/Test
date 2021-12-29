37e2eac0984bf27e5e2b5827a5af01ef4d0146de
udayKumarraj <96763452+udayKumarraj@users.noreply.github.com>
2021-12-29 10:51:47 +0530

    new update
    i have changed the code

--- myproject2/src/myproject2/Demo.java
+++ myproject2/src/myproject2/Demo.java
@@ -3,8 +3,11 @@ package myproject2;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hai");
+		int n=5;
+		if(n%2==0)
+			System.out.println("even number");
+		else
+			System.out.println("odd number);
 	}
 
 }
